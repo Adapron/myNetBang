ea5c5887e281f81c29e5afa4213eb5edaa494b1c
Adapron <adam.pavel.ap@gmail.com>
2023-12-31 14:05:02 +0100

    a lot of things

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.use(express.static('public', { 'extensions': ['html'] }));
-app.use(express.static('public', { 'extensions': ['js'], 'setHeaders': (res, path, stat) => { res.set('Content-Type', 'application/javascript'); } }));
-
-app.listen(8080, () => {
-  console.log('Server is running on http://localhost:8080');
-});
